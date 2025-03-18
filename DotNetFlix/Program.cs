57cff77041c4e84b8792dc3d3f359811a2856c7c
Rob Keimig <Rob.Keimig@pinnstrat.com>
2025-03-17 19:03:43 -0500

    AWS SDK & initial S3 testing code

--- DotNetFlix/Program.cs
+++ DotNetFlix/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+string accessKey = Environment.GetEnvironmentVariable("AwsS3AccessKey") ?? throw new Exception("Not Specified.");
+string secretKey = Environment.GetEnvironmentVariable("AwsS3SecretAccessKey") ?? throw new Exception("Not Specified.");
+var awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+var s3Client = new AmazonS3Client(awsCredentials, RegionEndpoint.USEast1);
+
+try
+{
+    var response = await s3Client.ListBucketsAsync();
+
+    Console.WriteLine("Your S3 Buckets:");
+    foreach (S3Bucket bucket in response.Buckets)
+    {
+        Console.WriteLine($"- {bucket.BucketName} (Created on {bucket.CreationDate})");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error listing buckets: {ex.Message}");
+}
