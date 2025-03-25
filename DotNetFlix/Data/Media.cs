9ab4554c7b81f6c498ca3798ca1b5ac7eea962b6
Rob Keimig <Rob.Keimig@pinnstrat.com>
2025-03-25 16:28:58 -0500

    Cleanup pass

--- DotNetFlix/Data/Media.cs
+++ DotNetFlix/Data/Media.cs
@@ -1,8 +1,4 @@
 ﻿using System.Data.SQLite;
-using Amazon;
-using Amazon.Runtime;
-using Amazon.S3;
-using Amazon.S3.Transfer;
 using Dapper;
 
 namespace DotNetFlix.Data;
@@ -24,60 +20,6 @@ public class MediaTable
 
 public static class MediaDataExtensions
 {
-    
-
-    //public static void WarmupMedia(this SQLiteConnection sql, long mediaId)
-    //{
-    //    Media media = default;
-        
-    //    lock (WarmupSyncRoot)
-    //    {
-    //        media = sql.GetMedia(mediaId);
-
-    //        if (media.IsPending)
-    //        {
-    //            return;
-    //        }
-
-    //        var contentFile = Path.Combine(CachePath, media.ContentId.ToString("N") + ".mp4");
-    //        if (File.Exists(contentFile)) { return; }
-
-    //        sql.Execute($@"
-    //            UPDATE {MediaTable.TableName}
-    //            SET     [{nameof(MediaTable.IsPending)}] = 1
-    //            WHERE   [{nameof(MediaTable.Id)}] = @{nameof(MediaTable.Id)}", new
-    //        {
-    //            Id = mediaId
-    //        });
-    //    }
-
-    //    Directory.CreateDirectory(CachePath);
-    //    Directory.CreateDirectory(CachePreloadPath);
-
-    //    var contentPreloadFileEnc = media.ContentPreloadId.ToString("N") + ".mp4.enc";
-    //    var contentPreloadFile = Path.Combine(CachePreloadPath, media.ContentPreloadId.ToString("N") + ".mp4");
-    //    DownloadS3File(sql, media.ContentPreloadId, contentPreloadFileEnc);
-    //    Cryptography.DecryptFile(contentPreloadFileEnc, contentPreloadFile, media.EncryptionKey);
-
-    //    Task.Run(() =>
-    //    {
-    //        var contentFileEnc = media.ContentId.ToString("N") + ".mp4.enc";
-    //        var contentFile = Path.Combine(CachePath, media.ContentId.ToString("N") + ".mp4");
-    //        DownloadS3File(sql, media.ContentId, contentFileEnc);
-    //        Cryptography.DecryptFile(contentFileEnc, contentFile, media.EncryptionKey);
-    //    });
-    //}
-
-    static void DownloadS3File(this SQLiteConnection sql, Guid id, string targetPath)
-    {
-        var configuration = sql.GetConfiguration();
-        var s3ObjectName = id.ToString("N");
-        var awsCredentials = new BasicAWSCredentials(configuration.AwsS3AccessKey, configuration.AwsS3SecretKey);
-        var s3Client = new AmazonS3Client(awsCredentials, RegionEndpoint.USEast1);
-        var fileTransferUtility = new TransferUtility(s3Client);
-        fileTransferUtility.Download(targetPath, configuration.AwsS3BucketName, s3ObjectName);
-    }
-
     public static List<Media> GetMedia(this SQLiteConnection sql)
     {
         var rows = sql.Query<MediaTable>($@"
@@ -129,13 +71,12 @@ public static class MediaDataExtensions
             RETURNING [{nameof(MediaTable.Id)}]", Map(media));
 
         var mediaBlockSequence = 0;
-        var contentFileStream = new FileStream(transcodedFile, FileMode.Open, FileAccess.Read);
-        
-        //Create Media Blocks until we've consumed the entire stream.
-        while (await sql.CreateMediaBlock(media.Id, mediaBlockSequence++, contentFileStream)) ;
+        var transcodedFileStream = new FileStream(transcodedFile, FileMode.Open, FileAccess.Read);
 
-        contentFileStream.Close();
-        await contentFileStream.DisposeAsync();
+        while (await sql.CreateMediaBlock(media.Id, mediaBlockSequence++, transcodedFileStream));
+
+        transcodedFileStream.Close();
+        await transcodedFileStream.DisposeAsync();
 
         File.Delete(uploadedFile);
         File.Delete(transcodedFile);
